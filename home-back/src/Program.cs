a2569ccc5ed1066394f5d5eb2589533cde76145e
Kyle Cunnison <kylecunnison@gmail.com>
2022-02-06 01:35:41 +0100

    registered repostories

--- home-back/src/Program.cs
+++ home-back/src/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using src;
 using System.Configuration;
+using src.Repositories;
+using src.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,26 @@ services.AddDbContext<BarContext>(options =>
     options.UseNpgsql(connectionString));
 
 services.AddControllers();
+
+// Registering Repositories
+
+services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+services.AddTransient<IBrandRepository, BrandRepository>();
+services.AddTransient<ICocktailRepository, CocktailRepository>();
+services.AddTransient<IFruitRepository, FruitRepository>();
+services.AddTransient<IGarnishRepository, GarnishRepository>();
+
+services.AddTransient<IGlassRepository, GlassRepository>();
+services.AddTransient<IIngredientRepository, IngredientRepository>();
+services.AddTransient<IInstructionsRepository, InstructionRepository>();
+services.AddTransient<ILiqueurRepository, LiqueurRepository>();
+services.AddTransient<ILiquorRepository, LiquorRepository>();
+
+services.AddTransient<IMeasurementRepository, MeasurementRepository>();
+services.AddTransient<IMeasurementTypeRepository, MeasurementTypeRepository>();
+services.AddTransient<IMixerRepository, MixerRepository>();
+services.AddTransient<ISyrupRepository, SyrupRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
